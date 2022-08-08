3ae2ae87907eb8842ab15dfcec4ad5c9b4e2bdc2
Beregovich <beregovich@mail.ru>
2022-08-08 12:00:02 +0300

    fix port

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(cookieParser());
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 try {
   bootstrap();
